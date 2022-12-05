ae0374b9fcc642b2aae334078a54388d05df9beb
Lorin Knudson <lcknudson@gmail.com>
2022-12-04 22:57:35 -0700

    file: orders.js complete for assessment

--- orders.js
+++ orders.js
@@ -31,6 +31,19 @@
 
 //CODE HERE
 
+class Ticket {
+    constructor(items, orderTime, customerId, status) {
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued';
+
+    }
+    updateStatus(newStatus) {
+        this.status = newStatus
+        console.log(`The order for ${this.customerId} is now ${this.status}`)
+    }
+}
 
 
 /*
@@ -46,6 +59,10 @@
 
 //CODE HERE
 
+const firstTicket = new Ticket (['pizza', 'bread', 'soda'], '7:03 PM', 575)
+
+console.log(firstTicket)
+
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +71,7 @@
 */
 
 //CODE HERE
+
+firstTicket.updateStatus('cooking')
+
+console.log(firstTicket)
